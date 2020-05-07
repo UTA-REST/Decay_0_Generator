be1425701804ff8e8e00eb997f6285eb0ed0a0f9
AustinMcDonald <austin.mcdonald96@mavs.uta.edu>
2020-05-07 16:59:47 -0500

    added batch

--- Batch_Decay0.cpp
+++ Batch_Decay0.cpp
@@ -6,9 +6,9 @@
 #include <gsl/gsl_integration.h>
 #include "decay0.h"
 //g++ -std=c++11 *.cpp -o Run_Decay0 -lgsl
-//g++ -std=c++11 Random.cpp XorShift256.cpp decay0.cpp Run_Decay0.cpp -o Run_Decay0 -lgsl
+//g++ -std=c++11 Random.cpp XorShift256.cpp decay0.cpp Batch_Decay0.cpp -o Batch_Decay0 -lgsl
 
-int main () 
+int main (int argc, char **argv) 
 {
     // Decay0 interface for BB decays ... (BB0nu: DecayMode 1), (BB2nu: DecayMode 4)  
     int _Xe136DecayMode = 4;
@@ -31,16 +31,11 @@ int main ()
        << " " << itp->_pmom[1] << " " << itp->_pmom[2] << std::endl;
     }
 
-    // this prints the presets in the generator
-    // seems kinda useless
-    //_decay0->printDecayModeList();
-
-
     //used this to make the output file. this thing is kinda slow...
     // took nearly an hour for 100,000
-    /* double Total_energy=0.0;
-    std::ofstream myfile ("bb2nu_energy.txt");
-    for(int i=1; i<=1000000; i++)
+    double Total_energy=0.0;
+    std::ofstream myfile (argv[1]);
+    for(int i=1; i<=100; i++)
     {
         std::cout<< i << std::endl;
         _decay0 = new decay0(XeName, _Ba136FinalState, _Xe136DecayMode);
@@ -49,15 +44,13 @@ int main ()
 
         for(std::vector<decay0Part>::const_iterator itp = theParts.begin(); itp != theParts.end(); itp++) 
         {
-        //std::cout << itp->_pdgCode << " " << itp->_energy << " " << itp->_time << " " << itp->_pmom[0] \
-        << " " << itp->_pmom[1] << " " << itp->_pmom[2] << std::endl;
             Total_energy+=itp->_energy;
         }
         myfile << Total_energy;
         myfile << "\n";
         Total_energy = 0.0;
     }
-    myfile.close(); */
+    myfile.close();
 
     return 0;
 }
